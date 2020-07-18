4904beaa6434546d25d729093e60cd308f3c5aaf
skunkwerks2020 <skunkwerks2020@gmail.com>
2020-07-17 21:38:34 -0600

    Updates

---
+++ precache-manifest.b2556d5809623b5ebb8cda4d7dcc4586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecf937861cd1a80bab57d3fb4cfe108",
+    "url": "/rwios-04-lp/index.html"
+  },
+  {
+    "revision": "6b87b17cbfce2dfe371b",
+    "url": "/rwios-04-lp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e909b645cf5394ca278",
+    "url": "/rwios-04-lp/static/js/2.e2da1249.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rwios-04-lp/static/js/2.e2da1249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b87b17cbfce2dfe371b",
+    "url": "/rwios-04-lp/static/js/main.88d30e98.chunk.js"
+  },
+  {
+    "revision": "36aa3f924404cdb4117c",
+    "url": "/rwios-04-lp/static/js/runtime-main.39ab63fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rwios-04-lp/static/media/logo.5d5d9eef.svg"
+  }
+]);
